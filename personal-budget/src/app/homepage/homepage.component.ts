86a5bb256e4531a4c843788ec6a67d3657ab8a23
o-serina <so88.opy@gmail.com>
2024-09-23 11:04:13 -0400

    fixed error on homepage

--- personal-budget/src/app/homepage/homepage.component.ts
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import Chart from 'chart.js/auto';
+
 @Component({
   selector: 'pb-homepage',
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.scss'
+  styleUrl: './homepage.component.scss',
 })
-export class HomepageComponent implements OnInit{
+export class HomepageComponent implements OnInit {
+  public dataSource: any = {
+    datasets: [
+      {
+        data: [],
+        backgroundColor: [
+          '#ffcd56',
+          '#ff6384',
+          '#36a2eb',
+          '#fd6b19',
+          '#83FF33',
+          '#F633FF',
+          '#FF3333',
+          '#a38080',
+        ],
+      },
+    ],
+    labels: [],
+  };
 
-  constructor(private http: HttpClient) {
-
-  }
+  constructor(private http: HttpClient) {}
 
   createChart() {
     const ctx = <HTMLCanvasElement>document.getElementById('myChart');
@@ -20,4 +38,5 @@ export class HomepageComponent implements OnInit{
     });
   }
 
+  ngOnInit(): void {}
 }
